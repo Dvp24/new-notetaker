8a5bdca563e5a4cf068b874a1d3c9c7660190372
Dhanashree <dvphalak@gmail.com>
2018-11-03 14:23:29 -0400

    newenrty

--- public/assets/js/add.js
+++ public/assets/js/add.js
@@ -4,7 +4,7 @@
   }).then(function( data){
     console.log(data)
     for (var i = 0; i < data.length; i++) {
-      var $listItem = $("<li class='list-group-item mt-4'>");
+      var $listItem = $("<p class=' mt-4'>");
       $listItem.append(
         $("<h6>").text("Note #" + (i + 1)+" "+ data[i].name).addClass("note"+i),
       );
